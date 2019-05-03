81833ac17787be1bf1fc3fafd430fa8fe4f0605e
qq <cumtqiangqiang@163.com>
2019-05-03 14:51:04 +0800

    LRModel

--- src/main/scala/cn/qiang/recommend/LRModel1.scala
+++ src/main/scala/cn/qiang/recommend/LRModel1.scala
@@ -1,10 +1,12 @@
 package cn.qiang.recommend
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.sql.SparkSession
 
 object LRModel1 {
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     val spark = SparkSession.builder()
       .appName("LRModel1")
       .master("local")
@@ -16,7 +18,17 @@ object LRModel1 {
       .setRegParam(0.3)
       .setElasticNetParam(0.8)
     val lrModel = lr.fit(training)
-    println(lrModel.coefficientMatrix)
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+    val mlr = new LogisticRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setFamily("multinomial")
+
+    val mlrModel = mlr.fit(training)
+    println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
+    println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
+
 
   }
 }
